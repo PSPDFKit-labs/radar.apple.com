696bbd66d82fe766b8f2a742c989cbec45720d71
Michael Ochs <mochs@bitecode.org>
2016-06-03 17:55:27 +0200

    update sample 26621519 - render same text all the time to don't overcomplicate things and have no side effects that could be seen as a bug

--- 26621519 - SelfSizingCells/SelfSizingCells/ViewController.m
+++ 26621519 - SelfSizingCells/SelfSizingCells/ViewController.m
@@ -10,9 +10,19 @@
 
 @interface ViewController ()
 
+@property (nonatomic, readonly) NSMutableDictionary *textCache;
+
 @end
 
 @implementation ViewController
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        _textCache = [NSMutableDictionary new];
+    }
+    return self;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -31,7 +41,11 @@
     cell.imageView.image = [UIImage imageNamed:@"document"];
     cell.textLabel.numberOfLines = 0;
 
-    NSString *text = [@"" stringByPaddingToLength:(NSUInteger)(rand() / (float)RAND_MAX * 100.0)+50 withString:@"Bacon ipsum dolor amet spare ribs drumstick hamburger prosciutto, fatback leberkas chuck pork chop. Capicola shoulder pork chop swine meatball chuck biltong rump jowl picanha shankle t-bone leberkas tail pork belly. Prosciutto meatloaf biltong, chicken ground round short loin filet mignon pig sausage meatball corned beef alcatra porchetta andouille fatback. Tail filet mignon ball tip flank ground round, tenderloin jerky beef capicola swine rump. Sirloin ground round short ribs spare ribs boudin tail pastrami meatloaf beef bacon ham hock." startingAtIndex:0];
+    NSString *text = self.textCache[@(indexPath.row)];
+    if (!text) {
+        text = [@"" stringByPaddingToLength:(NSUInteger)(rand() / (float)RAND_MAX * 100.0)+50 withString:@"Bacon ipsum dolor amet spare ribs drumstick hamburger prosciutto, fatback leberkas chuck pork chop. Capicola shoulder pork chop swine meatball chuck biltong rump jowl picanha shankle t-bone leberkas tail pork belly. Prosciutto meatloaf biltong, chicken ground round short loin filet mignon pig sausage meatball corned beef alcatra porchetta andouille fatback. Tail filet mignon ball tip flank ground round, tenderloin jerky beef capicola swine rump. Sirloin ground round short ribs spare ribs boudin tail pastrami meatloaf beef bacon ham hock." startingAtIndex:0];
+        self.textCache[@(indexPath.row)] = text;
+    }
     cell.textLabel.text = text;
 
     return cell;
