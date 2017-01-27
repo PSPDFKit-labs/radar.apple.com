d9994a4119ae982b6333a0f03e5ae7e67b568d54
Konstantin Bender <konstantin@pspdfkit.com>
2017-01-27 10:02:50 +0100

    Add 30230767 - SystemHeaderWarningsExample

---
+++ 30230767 - SystemHeaderWarningsExample/SystemHeaderWarningsExample/SystemHeaderWarningsExample.h
@@ -0,0 +1,17 @@
+//
+//  SystemHeaderWarningsExample.h
+//  SystemHeaderWarningsExample
+//
+//  Created by Konstantin @ PSPDFKit on 1/26/17.
+//  Copyright © 2017 PSPDFKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SystemHeaderWarningsExample.
+FOUNDATION_EXPORT double SystemHeaderWarningsExampleVersionNumber;
+
+//! Project version string for SystemHeaderWarningsExample.
+FOUNDATION_EXPORT const unsigned char SystemHeaderWarningsExampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SystemHeaderWarningsExample/PublicHeader.h>
